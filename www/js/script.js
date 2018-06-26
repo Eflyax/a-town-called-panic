e2395c319460cff32808b4df9203ed6c6f8a5221
Jakub Záruba <eflyax42@gmail.com>
2018-06-26 10:37:28 +0200

    URL Anchor

--- www/js/script.js
+++ www/js/script.js
@@ -12,5 +12,12 @@ $(document).ready(function () {
         var soundSlug = $(this).data('name');
         $('#' + soundSlug)[0].play();
         ga('send', 'event', 'Sound', 'play', soundSlug);
+        window.location.hash = soundSlug;
     });
 });
+
+var slugInUrl = $(location).attr('hash');
+if (slugInUrl) {
+    slugInUrl = slugInUrl.replace('#', '');
+    $('#' + slugInUrl)[0].play();
+}
