d2bbcead24b8ac70442c8c70104b38957d57693b
Jakub Záruba <eflyax42@gmail.com>
2018-06-26 10:23:15 +0200

    Added dependency on dg-ftpDeployment

--- www/js/script.js
+++ www/js/script.js
@@ -1,6 +1,16 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'UA-121383233-1');
+
 $(document).ready(function () {
     $('.square').on('click', function () {
         var soundSlug = $(this).data('name');
         $('#' + soundSlug)[0].play();
+        ga('send', 'event', 'Sound', 'play', soundSlug);
     });
 });
